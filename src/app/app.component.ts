93a1d09a07a142e07bb0dde7a85330ed11e309ad
Hong Hu <huhong215@gmail.com>
2018-10-10 11:25:58 -0400

    added css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular6SpringBoot-Client-StockMarket';
+  title = 'Financial Risk Management';
 }
